7074f8fe50b0932ae569d4646f320f1f45e35385
zhouyu <zhouyuzf@163.com>
2023-08-04 11:31:31 +0800

    fix bug: number need double quotes in annoations or labels
    Signed-off-by: zhouyu <zhouyuzf@163.com>

---
+++ pkg/template/string_validator.go
@@ -0,0 +1,21 @@
+package template
+
+import (
+	"strconv"
+	"strings"
+)
+
+type String string
+
+func (s String) IsBool() bool {
+	return s == "true" || s == "false"
+}
+
+func (s String) IsDecimal() bool {
+	_, err := strconv.Atoi(string(s))
+	return err == nil
+}
+
+func (s String) HasNewLine() bool {
+	return strings.Contains(string(s), "\n")
+}
